4c9c8a67bf8e0d7a765b25c50fa4e278b1ff85b7
tornadoHunter <apinchuk@hushmail.com>
2013-12-19 20:30:35 -0800

    Frist commit

---
+++ GameWindow.java
@@ -0,0 +1,33 @@
+import javax.swing.*; 
+import java.awt.*; 
+
+public class GameWindow { 
+    private final String GAME_NAME = "Tetris"; 
+    public static final int FRAME_SIZE_X = 360; 
+    public static final int FRAME_SIZE_Y = 720; 
+     
+    public GameWindow() { 
+
+        JFrame gameWindow = new JFrame(GAME_NAME); 
+        gameWindow.setSize(FRAME_SIZE_X, FRAME_SIZE_Y); 
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        JPanel grid = new JPanel(new GridLayout(1, 1, 1, 1)); 
+        Action action = new Action(); 
+        grid.add(action);
+//        grid.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+        gameWindow.add(grid); 
+        gameWindow.setVisible(true); 
+    }
+    
+    public int getFrameWidth() {
+        return FRAME_SIZE_X;
+    } 
+
+    public int getFrameHeight() {
+        return FRAME_SIZE_Y;
+    } 
+    
+} 
+    
+    
+
