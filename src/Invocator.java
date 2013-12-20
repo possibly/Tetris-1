6eb039a59244ae424efe76f0c7ee6b1cc310ce0e
tornadoHunter <apinchuk@hushmail.com>
2013-12-19 20:35:36 -0800

    Second commit

---
+++ src/Invocator.java
@@ -0,0 +1,12 @@
+/** 
+    This class contains a Game class invocation and 
+    a main method and starts the whole game.  
+*/ 
+
+public class Invocator { 
+    public static void main(String[] args) { 
+        new GameWindow(); 
+    } 
+} 
+
+
